c9c7a18ed0db25099e537b9282279bb56c1a7023
José Sancho <jsancho@dekalabs.com>
2022-08-01 20:02:30 +0200

    Create Xcode project

---
+++ PokemonWorld/PokemonWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWorldApp.swift
+//  PokemonWorld
+//
+//  Created by José Sancho on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
